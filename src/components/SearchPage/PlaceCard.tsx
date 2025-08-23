10108a3db3c7594bef50dfcb5036df6231f72a27
ChaeEunCha <cherrycha03@gmail.com>
2025-08-23 18:03:58 +0900

    modify: 검색 페이지 수정내용 구현

--- src/components/SearchPage/PlaceCard.tsx
+++ src/components/SearchPage/PlaceCard.tsx
@@ -1,11 +1,9 @@
+
 // import Food from '../../../assets/foodtype.svg?react';
-//import EASY from '../../../assets/L_EASY.svg?react';
-// import NORMAL from '../../../assets/L_NORMAL.svg?react';
-// import HARD from '../../../assets/L_HARD.svg?react';
-// import Food from '../../../assets/foodtype.svg?react';
-// import EASY from '../../../assets/L_EASY.svg?react';
-// import NORMAL from '../../../assets/L_NORMAL.svg?react';
-// import HARD from '../../../assets/L_HARD.svg?react';
+import NONE from '../../assets/TagNONE.svg?react'
+import EASY from '../../assets/TagEASY.svg';
+import NORMAL from '../../assets/TagNORMAL.svg?react';
+import HARD from '../../assets/TagHARD.svg?react';
 import WithIcon from '../../assets/with.svg?react';
 
 type PlaceCardProps = {
@@ -20,7 +18,7 @@ const PlaceCard = ({ imageUrl, title, location, tel, comment }: PlaceCardProps) 
   return (
     <div
       className="flex w-full h-[92px] bg-[var(--white-2,#FFFFFD)] items-center px-6 relative z-0"
-      style={{ borderBottom: '1px solid var(--PrimaryColor-2,#FFCEAA)' }}>
+      style={{ borderBottom: '1px solid #F78938' }}>
       <div className="relative w-[108px] h-[80px] shrink-0 -translate-y-1">
         {imageUrl ? (
           <img
@@ -37,6 +35,7 @@ const PlaceCard = ({ imageUrl, title, location, tel, comment }: PlaceCardProps) 
         <div className="flex items-center gap-2 text-black font-medium text-[16px] leading-[18px] font-[Pretendard]">
           {title}
           <WithIcon className="w-4 h-4 text-[#F78938]" />
+          
         </div>
         <div className="flex flex-col gap-1">
           <div className="text-[12px] leading-[14px] font-normal font-[Pretendard] text-black">
