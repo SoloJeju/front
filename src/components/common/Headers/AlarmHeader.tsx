b2331b67e58eae8762c3df08145627e808139dbe
somin2352 <somin030502@gmail.com>
2025-08-24 00:02:38 +0900

    Modify: 홈페이지에서 상단바 레이아웃 추가

--- src/components/common/Headers/AlarmHeader.tsx
+++ src/components/common/Headers/AlarmHeader.tsx
@@ -8,7 +8,11 @@ interface AlarmHeaderProps {
   showTitle?: boolean;
 }
 
-const AlarmHeader = ({ title, showLogo = false,  showTitle = true }: AlarmHeaderProps) => {
+const AlarmHeader = ({
+  title,
+  showLogo = false,
+  showTitle = true,
+}: AlarmHeaderProps) => {
   const navigate = useNavigate();
 
   const handleClickAlarm = () => {
@@ -16,13 +20,15 @@ const AlarmHeader = ({ title, showLogo = false,  showTitle = true }: AlarmHeader
   };
 
   return (
-    <header className="w-full py-3 flex items-center justify-between">
+    <header className="fixed top-0 max-w-[480px] w-full py-3 flex items-center justify-between bg-white z-50">
       <div className="flex justify-start">
         {showLogo && <img src={Logo} alt="로고" />}
       </div>
       <div className="flex justify-center">
         {showTitle && (
-          <span className="text-black-2 text-lg font-semibold font-['Pretendard'] leading-relaxed">{title}</span>
+          <span className="text-black-2 text-lg font-semibold font-['Pretendard'] leading-relaxed">
+            {title}
+          </span>
         )}
       </div>
       <div className="w-6 h-6 flex justify-end">
