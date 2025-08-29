d82ba0d482f0117a6ec5692ac40b867971dbf7f8
somin2352 <somin030502@gmail.com>
2025-08-29 15:41:12 +0900

    refactor: 무한스크롤 관련 타입 정의
    - 알림 목록 타입 재정의
    - 무한스크롤 commonType 정의

--- src/types/alarm.ts
+++ src/types/alarm.ts
@@ -1,4 +1,4 @@
-import type { CommonResponse } from './common';
+import type { CommonResponse, CommonCursorResponse } from './common';
 
 // 내 알림 조회
 export type MyNoti = {
@@ -11,10 +11,8 @@ export type MyNoti = {
   createdAt: Date;
 };
 
-export type ResponseMyNotiListDto = CommonResponse<{
-  notifications: MyNoti[];
-  hasUnread: boolean;
-  totalCount: number;
+export type ResponseMyNotiListDto = CommonCursorResponse<{
+  content: MyNoti[];
 }>;
 
 // 미확인 알림 여부
