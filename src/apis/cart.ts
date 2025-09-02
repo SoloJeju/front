ebe319ff29baa2aa1d8c7821ce825890b2c41547
anit7215 <tina112703@gmail.com>
2025-09-02 17:09:59 +0900

    Modify: axiosInstance 사용

--- src/apis/cart.ts
+++ src/apis/cart.ts
@@ -1,29 +1,29 @@
-import axios from 'axios';
+import { authAxios } from './axios';
 import type { CartListResponse } from '../types/cart';
 
 export const getCartList = async (): Promise<CartListResponse> => {
-  const { data } = await axios.get('/api/spot-cart');
+  const { data } = await authAxios.get('/api/spot-cart');
   return data;
 };
 
 export const addToCart = async (contentId: number, sortOrder = 1) => {
-  const { data } = await axios.post('/api/spot-cart', {contentId, sortOrder});
+  const { data } = await authAxios.post('/api/spot-cart', {contentId, sortOrder});
   return data;
 };
 
 export const deleteCartItem = async (cartId: number) => {
-  const { data } = await axios.delete(`/api/spot-cart/${cartId}`);
+  const { data } = await authAxios.delete(`/api/spot-cart/${cartId}`);
   return data;
 };
 
 export const bulkDeleteCart = async (cartIds: number[]) => {
-  const { data } = await axios.delete('/api/spot-cart/bulk', {
+  const { data } = await authAxios.delete('/api/spot-cart/bulk', {
     data: { cartIds },
   });
   return data;
 };
 
 export const clearCart = async () => {
-  const { data } = await axios.delete('/api/spot-cart');
+  const { data } = await authAxios.delete('/api/spot-cart');
   return data;
 };
