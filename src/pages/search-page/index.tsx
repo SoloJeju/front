9b1f1e70f4a40d0212c192f914217301c6ee43f3
somin2352 <somin030502@gmail.com>
2025-09-05 22:16:28 +0900

    modify: 경로 이동 시 contentTypeId 전달하도록 수정

--- src/pages/search-page/index.tsx
+++ src/pages/search-page/index.tsx
@@ -41,29 +41,43 @@ const SearchPage = () => {
     return () => observer.disconnect();
   }, [loader, hasNextPage, fetchNextPage, isFetchingNextPage]);
 
-  const handleCardClick = (id: string) => {
-    navigate(`/search-detail/${id}`);
+  const handleCardClick = (id: string, typeId: string) => {
+    navigate(`/search-detail/${id}`, {
+      state: {
+        contentTypeId: typeId,
+      },
+    });
   };
-// 로딩 스피너 만들어!!!!
+  // 로딩 스피너 만들어!!!!
   if (isLoading) return <div className="text-center p-4">로딩 중...</div>;
-  if (isError) return <div className="text-center p-4">오류가 발생했습니다.</div>;
+  if (isError)
+    return <div className="text-center p-4">오류가 발생했습니다.</div>;
 
   return (
     <div>
       <FilterBar />
       <div className="shrink-0 rounded-t-[12px] border-t border-[var(--PrimaryColor,#F78938)] bg-[var(--white-2,#FFFFFD)] mt-4 relative">
         <div className="mt-4 flex justify-center">
-          <button className="absolute left-1/2 -translate-x-1/2 -top-[15px] w-[108px] h-[30px] justify-center items-center gap-[4px] flex flex-shrink-0 rounded-[20px] border border-[#F78938] bg-[#FFFFFD] text-black text-[12px] font-medium leading-[30px] tracking-[-0.24px] font-[Pretendard]"
-            onClick={() => navigate('/search-map')}>
-            <Menu className="text-[#F78938] w-[20px] h-[20px]"/>지도로 보기
+          <button
+            className="absolute left-1/2 -translate-x-1/2 -top-[15px] w-[108px] h-[30px] justify-center items-center gap-[4px] flex flex-shrink-0 rounded-[20px] border border-[#F78938] bg-[#FFFFFD] text-black text-[12px] font-medium leading-[30px] tracking-[-0.24px] font-[Pretendard]"
+            onClick={() => navigate('/search-map')}
+          >
+            <Menu className="text-[#F78938] w-[20px] h-[20px]" />
+            지도로 보기
           </button>
         </div>
 
         <PlaceList spots={spots} onCardClick={handleCardClick} />
 
         <div ref={loader} style={{ height: 50 }} />
-        {isFetchingNextPage && <p className="text-center p-4">더 불러오는 중...</p>}
-        {!hasNextPage && spots.length > 0 && <p className="text-center text-gray-500 pb-4">더 이상 데이터가 없습니다</p>}
+        {isFetchingNextPage && (
+          <p className="text-center p-4">더 불러오는 중...</p>
+        )}
+        {!hasNextPage && spots.length > 0 && (
+          <p className="text-center text-gray-500 pb-4">
+            더 이상 데이터가 없습니다
+          </p>
+        )}
       </div>
       <div className="flex justify-end">
         <button
