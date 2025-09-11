b87c238eef7feb2a2481a4a01c7ba8e8d34f31e5
somin2352 <somin030502@gmail.com>
2025-09-11 22:46:53 +0900

    modify:알림리스트  그룹 알림 api 연결로 변경

--- src/types/alarm.ts
+++ src/types/alarm.ts
@@ -17,3 +17,22 @@ export type ResponseMyNotiListDto = CommonCursorResponse<{
 
 // 미확인 알림 여부
 export type ResponseUnreadNotiDto = CommonResponse<boolean>;
+
+// 내 알림 그룹 조회
+export type ResponseGroupedNotiDto = CommonResponse<{
+  content: [
+    {
+      type: string;
+      resourceType: string;
+      resourceId: number;
+      latestId: number;
+      latestMessage: string;
+      totalCount: number;
+      unreadCount: number;
+      latestCreatedAt: Date;
+    },
+  ];
+  nextCursor: string | null;
+  hasNext: boolean;
+  size: number;
+}>;
